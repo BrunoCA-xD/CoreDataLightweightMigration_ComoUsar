ed0f3f957394d0743a62bfbf043a801987ee1654
BrunoCA-xD <bruno.car.ambrosio@gmail.com>
2020-08-12 18:34:52 -0300

    Initial Commit

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Bruno Cardoso Ambrosio on 12/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
